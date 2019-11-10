e0964dd95510ea9a65aab94605fba3c413ea3386
KamDziok <kam_dziok@wp.pl>
2019-11-10 14:14:53 +0100

    Przykład rozwiązania dla ceny biletów

---
+++ model/Rezerwacje.php
@@ -0,0 +1,26 @@
+<?php
+
+class Rezerwacje{
+
+    var $cenaPodstawowaBiletu;
+    var $iloscNormalnych;
+    var $iloscUczenSenior;
+    var $iloscStudent;
+
+    function __construct($cenaPodstawowaBiletu, $iloscNormalnych, $iloscUczenSenior, $iloscStudent){
+        $this->cenaPodstawowaBiletu = $cenaPodstawowaBiletu;
+        $this->iloscNormalnych = $iloscNormalnych;
+        $this->iloscUczenSenior = $iloscUczenSenior;
+        $this->iloscStudent = $iloscStudent;
+    }
+
+
+    function Cena(){
+        $cena = 0.00
+        $cena += $this->cenaPodstawowaBiletu * $this->normalne;
+        $cena += $this->cenaPodstawowaBiletu * 0.7 * $this->iloscUczenSenior;
+        $cena += $this->cenaPodstawowaBiletu * 0.5 * $this->iloscStudent;
+        return round($cena, 2);
+    }
+
+}
