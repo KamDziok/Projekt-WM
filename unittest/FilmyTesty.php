573af3ce4d16c82014b8c3863b862943d79af7bb
Hubert Jakobsze <37272015+hubertj6@users.noreply.github.com>
2019-11-25 23:43:26 +0100

    Add files via upload

--- unittest/FilmyTesty.php
+++ unittest/FilmyTesty.php
@@ -4,13 +4,32 @@ include_once 'model/KolekcjaFilm.php';
 use PHPUnit\Framework\TestCase;
 class FilmyTest extends TestCase {
     public function testFilmu(){
-        $tytul="jacekPlacek";
-        $rezyser="a ktos";
-        $opis=" traratatata";
-        $film = new Film($tytul, $rezyser, $opis);
  
-            $expected = [$tytul, $rezyser, $opis];
-         $this->assertInternalType('String', $expected);
+                    $film = ['JacekPlacek', 'a', 'traratatata'];
+             
+                    for($i = 0; $i < count($film); $i++){
+                        $this->assertIsString( $film[$i]);
+                    }
+          
+        }
+        public function testFilmuliczba(){
+        
+                        try{
+                          $film = new Film(1, 1, 1);
+                    }catch(Exception $e){
+                        $this->assertEquals('argumet jest liczba', $e->getMessage());
+                    }
+ 
+        }
+        public function testFilmunull(){
+      
+                        try{
+                           $film= new Film (null, null, null);
+
+                    }catch(Exception $e){
+                        $this->assertEquals('argument nie został podany', $e->getMessage());
+                    }
+ 
         }
         
     }
