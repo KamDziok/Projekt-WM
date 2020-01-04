9c8318d319f80cdc4f81a5ac294767ec6a4976a3
Hubert Jakobsze <37272015+hubertj6@users.noreply.github.com>
2020-01-04 05:06:14 +0100

    Add files via upload

---
+++ interfejs/kup.php
@@ -0,0 +1,78 @@
+<?php
+
+	?>
+<html>
+<head>
+<meta http-equiv="Content-type" content="../text/html; charset=iso-8859-2" />
+<meta http-equiv="Reply-to" content="sidd@d4u.biz" />
+<meta http-equiv="Content-Language" content="pl" />
+<meta name="Author" content="Sidd" />
+<link rel="stylesheet" href="../stylesheet.css" type="text/css" /></head>
+<body leftmargin="0" rightmargin="0" topmargin="0" bottommargin="0"  marginwidth="0" marginheight="0">
+
+<table cellpadding="0" cellspacing="0"  align="center" style=" height:100%;">
+	<tr>
+	<td style="background:url(../images/p2.jpg); width:24px" valign="top"><img width="24"  src="../images/p1.jpg" /></td>
+		<td valign="top"><table style="width:672px; height:100%;" cellpadding="0" cellspacing="0">
+		            <tr><td><img src="../images/s3.jpg" /></td></tr>
+					<tr><td style="text-align: center"><span style="color:red; font-size: xx-large"  align="center"> Aplikacja Filmowa</span></td></tr>
+					
+					
+					<tr><td style="background:url(../images/top3.jpg); width:672px; height:25px; text-align:right; padding-right:60">
+					         <a> Twoje Konto</a> <input  type="button" value="Strona glowna" onClick="location.href='../index.php';" /> <br  />
+					</td></tr>
+					
+					<tr><td><img src="../images/top4.jpg" /></td></tr>
+					<tr><td valign="top" style="width:100px; height:100%; background-image:url(../images/s_tlo.jpg);">
+						
+                    <table>
+                    <a style="color:red; font-size: xx-large" aligen="center">   Wybrany film  </a>
+                   <td>
+<a style="font-size: medium " aligen="center"> dzien , godzina [poieranie z bazy dancyh]  </a>
+</td>
+<td>
+<a style="font-size: medium " aligen="center"> sala nr 2  </a>
+
+
+
+</td>
+</tr>
+<td colspan="2">
+
+
+<form action="*">
+<input type="radio" name="radio" value="wartość" />bilet normany 20 zł
+<input type="radio" name="radio" value="wartość" />bilet ulgowy 10 zł
+<input type="radio" name="radio" value="wartość" />bilet studencki 15 zł
+<input type="radio" name="radio" value="wartość" />bilet seniora 13 zł
+</form>
+
+</td>
+
+
+
+
+
+                    </table>
+                     
+                     <input style="background:url(images/se23.jpg); width:100px; color:red;" type="button" value="Kup Bilet" onClick="location.href='kup.php';" />
+						
+						</td>	
+                        
+						
+						<td></td>
+						<!-- dol -->
+						<tr><td><table cellpadding="0" cellspacing="0" >
+					
+						</table></td></tr>	
+	 		 </table>
+		</td>
+		<td style="background:url(../images/p2.jpg); width:24px" valign="top"><img width="24"  src="../images/p1.jpg" /></td>
+	</tr>
+
+
+</table>
+ 
+
+</body>
+</html>
