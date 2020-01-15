b13edd2efd53baf8a2333aa74e847c224c650669
KamDziok <kam_dziok@wp.pl>
2020-01-15 23:23:22 +0100

    Logowanie i przeniesienie diagramów

--- loadingPages/uzytkownik/logowanie.php
+++ loadingPages/uzytkownik/logowanie.php
@@ -16,12 +16,14 @@ header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
 $dataFormUser = json_decode(file_get_contents('php://input'));
+$dataToAppi = json_encode($dataFormUser);
 
-$ch->setPostURL($url, $dataFormUser);
+$ch->setPostURL($url, $dataToAppi);
 $rezult = $ch->exec();
 
 $json = json_decode($rezult, true);
-$login = $json->loginW;
+
+$login = $json['loginW'];
 
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
@@ -30,16 +32,37 @@ header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type
 
 if($login){
     try{
+        $test = true;
+        $rowInArray = array("id" => $json[0]['id_uzytkownika'], "date" => new DateTime());
         if(!file_exists("uzytkownicy.json")){
             $file = fopen("uzytkownicy.json","w");
-            $file->close();
+            fclose($file);
+            $arr_data = array();
+        }else{
+            $arr_data = file_get_contents("uzytkownicy.json");
+            $arr_data = json_decode($arr_data, true);
+            var_dump($arr_data);
+            for($i = 0; $i < count($arr_data); $i++){
+                echo $arr_data[$i]["id"] . " --- " . $json[0]['id_uzytkownika'] . " --- " . count($arr_data);
+                // if($arr_data[$i]["id"] === $json[0]['id_uzytkownika']){
+                if(strcmp($arr_data[$i]["id"], $json[0]['id_uzytkownika']) == 0){
+                    $arr_data[$i]['date'] == new DateTime();
+                    $test = false;
+                    break;
+                }
+            }
         }   
-        
-        $arr_data = json_decode("uzytkownicy.json", true);
 
-        for( $i = 0; $i < $arr_data; $i++){
-            echo $arr_data;
+        if($test){
+            array_push($arr_data, $rowInArray);
         }
+
+        var_dump($arr_data);
+        $putArray = json_encode($arr_data, true);
+        var_dump($putArray);
+        file_put_contents("uzytkownicy.json", $putArray);
+
+        echo json_encode($json[0], true);
 
     }catch(Exception $e){
         echo json_encode(array('message' => $e->getMessage));
