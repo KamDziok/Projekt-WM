d77e9176387b94a43560894d00339e11e359f40e
KamDziok <kam_dziok@wp.pl>
2020-01-15 11:54:34 +0100

    obsuga Uzytkownika

--- API/models/Uzytkownik.php
+++ API/models/Uzytkownik.php
@@ -2,7 +2,7 @@
     class Uzytkownik{
         //baza
         private $conn;
-        private $table = 'uzytkownicy';
+        private $table = 'user';
 
         //wlasciwosci uzytkownikow
         public $id_uzytkownika;
@@ -18,4 +18,72 @@
         }
 
         //funkcje z SQL...
+
+        public function getUzytkownicyAll(){
+            $query = 'SELECT user_id, uprawnienia_administracyjne, nick, mail, haslo, imie, nazwisko FROM ' . $this->table;
+
+            $stmt = $this->conn->prepare($query);
+
+            $stmt->execute();
+
+            return $stmt;
+        }
+
+        public function getUzytkownicyById(){
+            $query = 'SELECT user_id, uprawnienia_administracyjne, nick, mail, haslo, imie, nazwisko FROM ' . $this->table . ' WHERE id = ?';
+
+            $stmt = $this->conn->prepare($query);
+
+            //dodanie parametru
+            $stmt->BindParam(1, $this->id);
+
+            $stmt->execute();
+
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            $this->id = $row['id'];
+            $this->login = $row['login'];
+            $this->password = $row['password'];
+        }
+
+        public function create(){
+            $query = 'INSERT INTO ' . $this->table .'
+            SET 
+                user_id = :id,
+                nick = :login,
+                haslo = :password,
+                uprawnienia_administracyjne = :admin,
+                mail = :email,
+                imie = :imie,
+                nazwisko = :nazwisko;
+            ';
+
+            $stmt = $this->conn->prepare($query);
+
+            //czyszczenie danych
+            $this->id = htmlspecialchars(strip_tags($this->id));
+            $this->login = htmlspecialchars(strip_tags($this->login));
+            $this->password = htmlspecialchars(strip_tags($this->password));
+            $this->admin = htmlspecialchars(strip_tags($this->admin));
+            $this->email = htmlspecialchars(strip_tags($this->email));
+            $this->imie = htmlspecialchars(strip_tags($this->imie));
+            $this->nazwisko = htmlspecialchars(strip_tags($this->nazwisko));
+
+            $stmt->bindParam(':id', $this->id);
+            $stmt->bindParam(':login', $this->login);
+            $stmt->bindParam(':password', $this->password);
+            $stmt->bindParam(':admin', $this->admin);
+            $stmt->bindParam(':email', $this->email);
+            $stmt->bindParam(':imie', $this->imie);
+            $stmt->bindParam(':nazwisko', $this->nazwisko);
+
+            if($stmt->execute()){
+                return true;
+            }
+
+            //wypisz blad
+            printf('Error: %s.\n', $stmt->error);
+
+            return false;
+        }
     }
